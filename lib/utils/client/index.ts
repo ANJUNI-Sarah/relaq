c70a3e5b76a437b0b478f699f2d41dc08776508d
ANJUNI-Sarah <anjuni1996@gmail.com>
2025-08-02 16:42:19 +0800

    fix server domain

--- lib/utils/client/index.ts
+++ lib/utils/client/index.ts
@@ -14,7 +14,7 @@ export const client = axios.create({
  * 後端api的實例
  */
 export const server = axios.create({
-    baseURL: "http://localhost:8000/",
+    baseURL: process.env.NEXT_PUBLIC_SERVER_API_URL,
     headers: {
         "Content-Type": "application/json",
     },
